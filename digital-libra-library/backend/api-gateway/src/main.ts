dd3d5a2fc809a9e125153541d52a96ebab73d0d2
Ваше Ім’я <khomenko.oleg@hotmail.com>
2025-03-20 22:11:01 +0200

    Update main.ts

--- digital-libra-library/backend/api-gateway/src/main.ts
+++ digital-libra-library/backend/api-gateway/src/main.ts
@@ -5,12 +5,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-    app.connectMicroservice({
-      transport: Transport.TCP,
-      options: {
-        host: 'localhost',
-        port: 3001,
-      },
+  app.connectMicroservice({
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://localhost:5672'],
+      queue: 'user-service',
+      queueOptions: { durable: false },
+    },
   });
 
   await app.listen(3000);
